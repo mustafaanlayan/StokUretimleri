12ef5e8ccdefaf4d46861cbaf906c63dc2bcb30a
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-05 12:42:38 +0300

    commit sipariskaydi1

--- StokUretimProgramlari/FrmSiparişKaydi.cs
+++ StokUretimProgramlari/FrmSiparişKaydi.cs
@@ -88,6 +88,86 @@ namespace StokUretimProgramlari
            conn.Close();
         }
 
+         string x3="0";
+        void stokkontrol()
+        {
+            conn.Open();
+
+            SqlCommand sorgu1 = new SqlCommand("SELECT COUNT (*) FROM TBL_STOKKAYITLARI WHERE STOK_KODU='" + txtStokKodu.Text + "'", conn);
+            SqlDataReader dr = sorgu1.ExecuteReader();
+            while (dr.Read())
+            {
+                x3 = dr[0].ToString();
+            }
+            conn.Close();
+        }
+
+        void stokbilgisicekme()
+        {
+            conn.Open();
+
+            SqlCommand sorgu1 = new SqlCommand("SELECT STOK_ADI, FİYAT, KDV_ORANI FROM TBL_STOKKAYITLARI WHERE STOK_KODU='" + txtStokKodu.Text + "'", conn);
+            SqlDataReader dr1 = sorgu1.ExecuteReader();
+            while (dr1.Read())
+            {
+                txtStokAdi.Text = dr1[0].ToString();
+                txtFiyat.Text = dr1[1].ToString();
+                txtKdv.Text = dr1[2].ToString();
+            }
+            conn.Close();
+        }
+
+
+        void geneltoplamhesaplama()
+        {
+            conn.Open();
+
+            SqlCommand sorgu1 = new SqlCommand("SELECT SUM(MİKTAR*FİYAT)*((KDV/100)+1) FROM TBL_SIPARISKALEMLERİ WHERE SIPARIS_NO='"+txtSiparisNo.Text+"'GROUP BY SIPARIS_NO" , conn);
+            SqlDataReader dr1 = sorgu1.ExecuteReader();
+            while (dr1.Read())
+            {
+                txtToplamTutar.Text = dr1[0].ToString();
+            }
+            conn.Close();
+        }
+
+        void temizle1()
+        {
+            txtStokKodu.Text = "";
+            txtStokAdi.Text = "";
+            txtUrunAciklamasi.Text = "";
+            txtFiyat.Text = "";
+            txtKdv.Text = "";
+            txtMiktar.Text = "";
+        }
+
+        void temizle2()
+        {
+            txtSiparisTarihi.Text = "";
+            txtTeslimTarihi.Text = "";
+            txtMusteriKodlari.Text = "";
+            txtMusteriAdlari.Text = "";
+            txtIl.Text = "";
+            txtIlce.Text = "";
+            txtToplamTutar.Text = "";
+            temizle1();
+
+        }
+        string x6 = "0";
+
+        void sipgenelisemrikontrol()
+        {
+            conn.Open();
+
+            SqlCommand sorgu1 = new SqlCommand("SELECT", conn);
+            SqlDataReader dr1 = sorgu1.ExecuteReader();
+            while (dr1.Read())
+            {
+                txtToplamTutar.Text = dr1[0].ToString();
+            }
+            conn.Close();
+        }
+
         void mustertibilgisicekme()
         {
             conn.Open();
